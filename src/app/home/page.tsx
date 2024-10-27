9db3e10218fbb9ff09ab510cb005c3678653b920
Ivan Uskov <yskovivan2001@gmail.com>
2024-10-27 12:32:21 +0300

    add translator

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,12 @@
 import SideBar from '@/components/sideBar/SideBar';
 import React from 'react';
 import classes from './page.module.css';
-import {FaBook} from 'react-icons/fa';
-import {SiSimilarweb, SiMicrosofttranslator} from 'react-icons/si';
-import {AiFillSound, AiFillRead} from 'react-icons/ai';
-import {BsFillPencilFill} from 'react-icons/bs';
+import { FaBook } from 'react-icons/fa';
+import { SiSimilarweb, SiMicrosofttranslator } from 'react-icons/si';
+import { AiFillSound, AiFillRead } from 'react-icons/ai';
+import { BsFillPencilFill } from 'react-icons/bs';
 
-const page = ():JSX.Element => {
+const page = (): JSX.Element => {
   return (
     <div className={classes.home}>
       <SideBar>
@@ -30,10 +30,12 @@ const page = ():JSX.Element => {
           <AiFillRead />
           <span className={classes.text}>Reading</span>
         </div>
-        <div className={classes.navOption}>
-          <SiMicrosofttranslator />
-          <span className={classes.text}>Translator</span>
-        </div>
+        <a href="/translator">
+          <div className={classes.navOption}>
+            <SiMicrosofttranslator />
+            <span className={classes.text}>Translator</span>
+          </div>
+        </a>
       </SideBar>
     </div>
   );
---
+++ src/app/translator/page.tsx
@@ -0,0 +1,17 @@
+import SideBar from '@/components/sideBar/SideBar';
+import React from 'react';
+import classes from './page.module.css';
+import { FaBook } from 'react-icons/fa';
+import { SiSimilarweb, SiMicrosofttranslator } from 'react-icons/si';
+import { AiFillSound, AiFillRead } from 'react-icons/ai';
+import { BsFillPencilFill } from 'react-icons/bs';
+
+const page = (): JSX.Element => {
+    return (
+        <div className={classes.home}>
+            хуй
+        </div>
+    );
+};
+
+export default page;
